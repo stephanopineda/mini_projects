e3d0ebd473c256d1c0972ad7d285cdd38ce9f9d9
Stephano Denniel Pineda <denniel.step@gmail.com>
2024-12-17 14:31:13 +0800

    Used pylint and resolved issues.
    This code is 10/10 in pylint. Coursera Python Project for Data Engineering introduced me in using it.

--- 5_project_bank_market/banks_project.py
+++ 5_project_bank_market/banks_project.py
@@ -1,18 +1,27 @@
+'''
+This code is an ETL code for webscraping specifically a webpage from wikipedia.
+This will extract a table, transform the data, and store it in csv and database.
+This is a project in coursera course "Python Project for Data Engineering by IBM."
+'''
 from datetime import datetime
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd 
-import sqlite3
+import pandas as pd
 
-project_path = './5_project_bank_market'
-log_file_path = '/code_log.txt'
+PROJECT_PATH = './5_project_bank_market'
+LOG_FILE_PATH = '/code_log.txt'
 
 def log_progress(message):
-    with open(project_path + log_file_path, 'a') as log_file:
+    ''' Saves message to a text log specified in the log path.
+    Args:
+        message (str): The message to be logged.
+    Returns:
+        None
+    '''
+    with open(PROJECT_PATH + LOG_FILE_PATH, 'a', encoding="utf-8") as log_file:
         log_file.write(f'{datetime.now().strftime('%Y-%h-%d %H:%M:%S')} : {message} \n')
         # print(f'{datetime.now().strftime('%Y-%h-%d %H:%M:%S')} {message}')
-    
-    return None
 
 def extract(url, table_attributes, table_index=0):
     ''' Extracts table from a web page using BeautifulSoup. Converts the table to a DataFrame.
@@ -25,21 +34,21 @@ def extract(url, table_attributes, table_index=0):
     Returns:
         pandas.DataFrame: The extracted table in DataFrame format. 
     '''
-    response = requests.get(url)
+    response = requests.get(url, timeout=30)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
 
     tables = soup.find_all('tbody')
     if table_index >= len(tables):
-        raise ValueError(f"Table index {table_index} is out of range. Found {len(tables)} <tbody> elements.")
+        raise ValueError(f"Index {table_index} is out of range. Found {len(tables)} <tbody>.")
     rows = tables[table_index].find_all('tr')
 
     data = []
 
     for row in rows:
         cols = row.find_all('td')
-        
-        if not cols: # If data is empty 
+
+        if not cols: # If data is empty
             continue
 
         data.append(
@@ -67,11 +76,10 @@ def transform(df, exchange_rate_csv_path, table_column_names):
     '''
     exchange_rate_df = pd.read_csv(exchange_rate_csv_path)
     exchange_rate = exchange_rate_df.set_index('Currency')['Rate'].to_dict()
-    
+
     df[table_column_names[2]] = round(df['MC_USD_Billion'] * exchange_rate['GBP'], 2)
     df[table_column_names[3]] = round(df['MC_USD_Billion'] * exchange_rate['EUR'], 2)
-    df[table_column_names[4]] = round(df['MC_USD_Billion'] * exchange_rate['INR'], 2) 
-    
+    df[table_column_names[4]] = round(df['MC_USD_Billion'] * exchange_rate['INR'], 2)
     print(df['MC_EUR_Billion'][4])
     return df
 
@@ -86,7 +94,6 @@ def load_to_csv(df, file_path):
         None
     '''
     df.to_csv(file_path, sep=',', encoding='utf-8', index=False)
-    return None
 
 def load_to_db(df, sql_conn, table_name):
     ''' Saves DataFrame into the specified database.
@@ -100,40 +107,52 @@ def load_to_db(df, sql_conn, table_name):
         None
     '''
     df.to_sql(table_name, sql_conn, if_exists='replace', index=False)
-    return None
 
 def run_query(query_statement, sql_connection):
+    '''Prints the passed SQL query and the output on the terminal.
+    Args:
+        query_statement (str): SQL Query to be executed.
+        sql_connection (Connection): Connection to the selected database.
+
+    Returns:
+        pandas.DataFrame: the query output as DataFrame 
+    '''
     print(query_statement)
     return pd.read_sql(query_statement, sql_connection)
 
 # Main Code
 
-url = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
-exchange_rate_path_csv = '/exchange_rate.csv'
-table_attributes = ['Name', 'MC_USD_Billion']
-table_attributes_final = ['Name', 'MC_USD_Billion', 'MC_GBP_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
+BANKS_URL = 'https://web.archive.org/web/20230908091635/\
+https://en.wikipedia.org/wiki/List_of_largest_banks'
+EXCHANGE_RATE_CSV_PATH = '/exchange_rate.csv'
+BANK_TABLE_ATTRIBS = ['Name', 'MC_USD_Billion']
+BANK_TABLE_ATTRIBS_FINAL = ['Name',
+                            'MC_USD_Billion', 
+                            'MC_GBP_Billion',
+                            'MC_EUR_Billion', 
+                            'MC_INR_Billion']
 
-csv_path = '/Largest_banks_data.csv'
-db_path = '/Banks.db'
-table_name = 'largest_banks'
+BANK_CSV_PATH = '/Largest_banks_data.csv'
+BANK_DB_PATH = '/Banks.db'
+BANK_TABLE_NAME = 'largest_banks'
 
 log_progress('Preliminaries complete. Initiating ETL process.')
-df = extract(url, table_attributes)
+banks_df = extract(BANKS_URL, BANK_TABLE_ATTRIBS)
 log_progress('Data extraction complete. Initiating Transformation process')
-df = transform(df, project_path+exchange_rate_path_csv, table_attributes_final)
+banks_df = transform(banks_df, PROJECT_PATH+EXCHANGE_RATE_CSV_PATH, BANK_TABLE_ATTRIBS_FINAL)
 log_progress('Data transformation complete. Initiating Loading process')
-load_to_csv(df, project_path+csv_path)
+load_to_csv(banks_df, PROJECT_PATH+BANK_CSV_PATH)
 log_progress('Data saved to CSV file')
-conn = sqlite3.connect(project_path+db_path)
+conn = sqlite3.connect(PROJECT_PATH+BANK_DB_PATH)
 log_progress('SQL Connection initiated')
-load_to_db(df, conn, table_name)
+load_to_db(banks_df, conn, BANK_TABLE_NAME)
 log_progress('Data loaded to Database as a table, Executing queries')
-query1 = 'SELECT * FROM Largest_banks'
-query2 = 'SELECT AVG(MC_GBP_Billion) FROM Largest_banks'
-query3 = 'SELECT Name from Largest_banks LIMIT 5'
-print(run_query(query1, conn))
-print(run_query(query2, conn))
-print(run_query(query3, conn))
+QUERY1 = 'SELECT * FROM Largest_banks'
+QUERY2 = 'SELECT AVG(MC_GBP_Billion) FROM Largest_banks'
+QUERY3 = 'SELECT Name from Largest_banks LIMIT 5'
+print(run_query(QUERY1, conn))
+print(run_query(QUERY2, conn))
+print(run_query(QUERY3, conn))
 log_progress('Process Complete')
 conn.close()
 log_progress('Server Connection closed')
